9e3d82d83d13f19c907e96b5b51ddecfc69f2a22
EstherDasolJung <lovenovel19@gmail.com>
2022-04-06 03:48:43 +0900

    초기 프로젝트 업로드
    초기 업로드
    캐릭터 이동 시도 중
    키보드 화살표 이동, switch case문 연습중

---
+++ Assets/Script/Hero_Ctrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero_Ctrl : MonoBehaviour
+{
+    KeyCode key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(key) //아 스위치문 쓰고싶다...
+        {
+            case KeyCode.RightArrow:
+                Debug.Log("오른쪽 화살표 입력");
+                break;
+        }
+    }
+}
