2c2537f7ae4adad344699085ec322d43ad351dbf
EstherDasolJung <103065745+EstherDasolJung@users.noreply.github.com>
2022-04-11 09:58:08 +0900

    Update Hero_Ctrl.cs
    이동 소스 수정

--- Assets/Script/Hero_Ctrl.cs
+++ Assets/Script/Hero_Ctrl.cs
@@ -5,6 +5,8 @@ using System.Linq;
 
 public class Hero_Ctrl : MonoBehaviour
 {
+    int speed = 10;
+
     //https://blog.naver.com/dusdkel/222532923128
     static private readonly KeyCode[] keyCodes = System.Enum.GetValues(typeof(KeyCode)).Cast<KeyCode>().Where(k => ((int)k < (int)KeyCode.Mouse0)).ToArray();
  
@@ -20,12 +22,10 @@ public class Hero_Ctrl : MonoBehaviour
         return null;
     }
 
-    Vector2 HeroPoint = Vector2.zero;
-
     // Start is called before the first frame update
     void Start()
     {
-        HeroPoint = this.gameObject.transform.position;
+    
     }
 
     // Update is called once per frame
@@ -37,12 +37,12 @@ public class Hero_Ctrl : MonoBehaviour
             {
                 case KeyCode.LeftArrow :
                     Debug.Log("왼쪽 화살표 클릭");
-                    HeroPoint.x -= 1.0f * Time.deltaTime;
+                    transform.Translate(new Vecter3(Speed * Time.deltatime,0,0));
                     break;
                     
                 case KeyCode.RightArrow :
                     Debug.Log("오른쪽 화살표 클릭");
-                    HeroPoint.x += 1.0f * Time.deltaTime;
+                    transform.Translate(new Vecter3(-Speed * Time.deltatime,0,0));
                     break;
                     
                 case KeyCode.UpArrow :
@@ -50,7 +50,7 @@ public class Hero_Ctrl : MonoBehaviour
                     //if(Uprope = false)
                     //    break;
                     
-                    HeroPoint.y += 1.0f * Time.deltaTime;
+                    transform.Translate(new Vecter3(0,Speed * Time.deltatime,0));
                     break;
                     
                 case KeyCode.DownArrow :
@@ -58,12 +58,12 @@ public class Hero_Ctrl : MonoBehaviour
                     //if(Downrope = false)
                     //    break;
 
-                    HeroPoint.y -= 1.0f * Time.deltaTime;
+                    transform.Translate(new Vecter3(0,-Speed * Time.deltatime,0));
                     break;
 
                 case KeyCode.LeftControl://KeyCode.RightControl :
                     Debug.Log("컨트롤 클릭");
-                    if(transform.position.y > 10.0f) //점프 제한 조건
+                    if(transform.position.y > 10.0f  ) //점프 제한 조건 : 일정 높이 이상 점프 불가
                         break;
             
                     //점프 구현하기 https://angliss.tistory.com/292?category=861686
@@ -72,83 +72,9 @@ public class Hero_Ctrl : MonoBehaviour
                 default :
                     //state(idle);
                     break;
+                    
+                    //추가해야 할것 : 바닥에 캐릭터가 닿기 전에 이동 제한(예 : 점프시 재 점프등)
             }
-        }
-        
-        /*
-        if(Input.GetKey(KeyCode.LeftArrow))
-            KeyBoardMove(1);
-        else if(Input.GetKey(KeyCode.RightArrow))
-            KeyBoardMove(2);
-        else if(Input.GetKey(KeyCode.UpArrow))
-            KeyBoardMove(3);
-        else if(Input.GetKey(KeyCode.DownArrow))
-            KeyBoardMove(4);
-        else if(Input.GetKey(KeyCode.LeftControl))
-            KeyBoardMove(5);
-        else if (Input.GetKey(KeyCode.RightControl))
-            KeyBoardMove(5);        
-        */
+        }      
     }
-    /*
-    void KeyBoardMove(int key)
-    {
-        switch(key)
-        {
-            case 1:
-                Debug.Log("왼쪽 화살표 클릭");
-                HeroPoint.x -= 1.0f * Time.deltaTime;
-                break;
-                
-            case 2:
-                Debug.Log("오른쪽 화살표 클릭");
-                HeroPoint.x += 1.0f * Time.deltaTime;
-                break;
-                
-            case 3:
-                Debug.Log("위쪽 화살표 클릭");
-                FloorUp();
-                break;
-                
-            case 4:
-                Debug.Log("아래쪽 화살표 클릭");
-                FloorDown();
-                break;
-            
-            case 5:
-                Debug.Log("컨트롤 클릭");
-                Jump();
-                break;
-                
-            default:
-                //state(idle);
-                break;
-            
-        }
-    }
-    
-    void FloorUp()
-    {
-        //if(Uprope = false)
-        //    return;
-
-        HeroPoint.y += 1.0f * Time.deltaTime;
-    }
-    
-    void FloorDown()
-    {
-        //if(Downrope = false)
-        //    return;
-
-        HeroPoint.y -= 1.0f * Time.deltaTime;
-    }
-    
-    void Jump()
-    {
-        if(transform.position.y > 10.0f)
-            return;
-            
-        //점프 구현하기 https://angliss.tistory.com/292?category=861686
-    }
-    */
 }
