dfaa7cce8b22ff0e118ca7e0e33de0119c6ed4fe
EstherDasolJung <103065745+EstherDasolJung@users.noreply.github.com>
2022-04-11 14:28:31 +0900

    Update Hero_Ctrl.cs
    캐릭터 점프 구현

--- Assets/Script/Hero_Ctrl.cs
+++ Assets/Script/Hero_Ctrl.cs
@@ -5,7 +5,9 @@ using System.Linq;
 
 public class Hero_Ctrl : MonoBehaviour
 {
-    int speed = 10;
+    public float m_speed = 10.0f;
+    public float m_JumpForce = 5.0f;
+    public bool m_Jump = false;
 
     //https://blog.naver.com/dusdkel/222532923128
     static private readonly KeyCode[] keyCodes = System.Enum.GetValues(typeof(KeyCode)).Cast<KeyCode>().Where(k => ((int)k < (int)KeyCode.Mouse0)).ToArray();
@@ -63,16 +65,22 @@ public class Hero_Ctrl : MonoBehaviour
                     Debug.Log("아래쪽 화살표 클릭");
                     //if(Downrope = false)
                     //    break;
-                    transform.posion += Vector3.Down * Speed;
+                    transform.posion += Vector3.Down;
                     //transform.Translate(new Vector3(0,-Speed * Time.deltatime,0));
                     break;
 
-                case KeyCode.LeftControl://KeyCode.RightControl :
+                case KeyCode.LeftControl:
                     Debug.Log("컨트롤 클릭");
-                    if(transform.position.y > 10.0f  ) //점프 제한 조건 : 일정 높이 이상 점프 불가
-                        break;
-            
-                    //점프 구현하기 https://angliss.tistory.com/292?category=861686
+                    //if(transform.position.y > 10.0f  ) //점프 제한 조건 : 일정 높이 이상 점프 불가
+                    //    break;
+                    if(m_Jump != true)
+                    {
+                        Rigidbody2D.rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+                        rigidbody2D.AddForce(Vector3.up * m_JumpForce);
+                        //점프 구현하기 https://angliss.tistory.com/292?category=861686
+                    
+                        m_Jump = true;
+                    }
                     break;
                     
                 default :
@@ -82,5 +90,9 @@ public class Hero_Ctrl : MonoBehaviour
                     //추가해야 할것 : 바닥에 캐릭터가 닿기 전에 이동 제한(예 : 점프시 재 점프등)
             }
         }      
+    }
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        m_Jump = false;
     }
 }
