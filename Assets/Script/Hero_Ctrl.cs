761b35c7e58877f18200cd116daa430d3b73f821
EstherDasolJung <lovenovel19@gmail.com>
2022-04-12 07:50:17 +0900

    테스트 및 일부 소스 수정
    점프 이동이 되지 않음 수정 필요,
    카메라 컨트롤러, 몬스터 컨트롤러 스크립트 추가

--- Assets/Script/Hero_Ctrl.cs
+++ Assets/Script/Hero_Ctrl.cs
@@ -5,9 +5,9 @@ using System.Linq;
 
 public class Hero_Ctrl : MonoBehaviour
 {
-    public float m_speed = 10.0f;
-    public float m_JumpForce = 5.0f;
-    public bool m_Jump = false;
+    float m_speed = 10.0f;
+    float m_JumpForce = 5.0f;
+    bool m_Jump = false;
 
     //https://blog.naver.com/dusdkel/222532923128
     static private readonly KeyCode[] keyCodes = System.Enum.GetValues(typeof(KeyCode)).Cast<KeyCode>().Where(k => ((int)k < (int)KeyCode.Mouse0)).ToArray();
@@ -40,7 +40,7 @@ public class Hero_Ctrl : MonoBehaviour
                 case KeyCode.LeftArrow :
                     Debug.Log("왼쪽 화살표 클릭");
                     this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
-                    transform.posion += Vector3.Left * Speed;
+                    transform.position += Vector3.left * m_speed * Time.deltaTime;
                     
                     //transform.Translate(new Vector3(Speed * Time.deltatime,0,0));
                     break;
@@ -48,7 +48,7 @@ public class Hero_Ctrl : MonoBehaviour
                 case KeyCode.RightArrow :
                     Debug.Log("오른쪽 화살표 클릭");
                     this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
-                    transform.posion += Vector3.Right * Speed;
+                    transform.position += Vector3.right * m_speed * Time.deltaTime;
                     
                     //transform.Translate(new Vector3(-Speed * Time.deltatime,0,0));
                     break;
@@ -57,7 +57,7 @@ public class Hero_Ctrl : MonoBehaviour
                     Debug.Log("위쪽 화살표 클릭");
                     //if(Uprope = false)
                     //    break;
-                    transform.posion += Vector3.Up * Speed;
+                    transform.position += Vector3.up * m_speed * Time.deltaTime;
                     //transform.Translate(new Vector3(0,Speed * Time.deltatime,0));
                     break;
                     
@@ -65,7 +65,7 @@ public class Hero_Ctrl : MonoBehaviour
                     Debug.Log("아래쪽 화살표 클릭");
                     //if(Downrope = false)
                     //    break;
-                    transform.posion += Vector3.Down;
+                    transform.position += Vector3.down * Time.deltaTime;
                     //transform.Translate(new Vector3(0,-Speed * Time.deltatime,0));
                     break;
 
@@ -75,10 +75,10 @@ public class Hero_Ctrl : MonoBehaviour
                     //    break;
                     if(m_Jump != true)
                     {
-                        Rigidbody2D.rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
-                        rigidbody2D.AddForce(Vector3.up * m_JumpForce);
+                        Rigidbody2D rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+                        GetComponent<Rigidbody2D>().AddForce(Vector3.up * m_JumpForce * Time.deltaTime);
                         //점프 구현하기 https://angliss.tistory.com/292?category=861686
-                    
+
                         m_Jump = true;
                     }
                     break;
