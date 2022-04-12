2b17fd01b1c835f997f1bb950dd4e837111f4689
EstherDasolJung <103065745+EstherDasolJung@users.noreply.github.com>
2022-04-12 18:09:17 +0900

    Update Camera_Ctrl.cs
    카메라가 캐릭터를 따라 다니도록 수정

--- Assets/Script/Camera_Ctrl.cs
+++ Assets/Script/Camera_Ctrl.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Camera_Ctrl : MonoBehaviour
 {
+    public GameObject m_MapObj;
+    public GameObject m_PlayerObj;
+    Vector3 cameraPosition = new Vector3(0, 0, -10);
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = m_PlayerObj.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    void FixedUpdate()
+    {
+    	transform.position = Vector3.Lerp(transform.position, playerTransform.position + cameraPosition, 
+                                  Time.deltaTime * cameraMoveSpeed);
     }
 }
