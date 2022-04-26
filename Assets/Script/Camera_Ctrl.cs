3581fdb40e48937b4c4bc78e5c20fd495de7d4f5
EstherDasolJung <103065745+EstherDasolJung@users.noreply.github.com>
2022-04-26 17:25:34 +0900

    Update Camera_Ctrl.cs
    1차 수정

--- Assets/Script/Camera_Ctrl.cs
+++ Assets/Script/Camera_Ctrl.cs
@@ -8,6 +8,20 @@ public class Camera_Ctrl : MonoBehaviour
     public GameObject m_PlayerObj;
     Vector3 cameraPosition = new Vector3(0, 0, -10);
     
+    [HideInInspector] public Vector3 GroundMin = Vector3.zero;
+    [HideInInspector] public Vector3 GroundMax = Vector3.zero;
+    
+    float m_LmtBdLeft = 0.0f;
+    float m_LmtBdRight = 0.0f;
+    float m_LmtBdTop = 0.0f;
+    float m_LmtBdBottom = 0.0f;
+    
+    Vector3 m_ScMin = new Vector3(0.0f,0.0f,0.0f); //ScreenViewPort 좌측하단
+    [HideInInspector] public Vector3 m_CamWMin = Vector3.zero;
+    Vector3 m_ScMax = new Vector3(1.0f,1.0f,1.0f); //ScreenViewPort 우측하단
+    [HideInInspector] public Vector3 m_CamWMax = Vector3.zero;
+    Vector3 m_ScWdHalf = Vector3.zero;
+    
     // Start is called before the first frame update
     void Start()
     {
