9d5593d0825a5763dec7cf378f633dfdc31f5516
EstherDasolJung <lovenovel19@gmail.com>
2022-05-23 16:30:26 +0900

    22.05.23
    22.05.23

--- Assets/Script/Camera_Ctrl.cs
+++ Assets/Script/Camera_Ctrl.cs
@@ -7,7 +7,9 @@ public class Camera_Ctrl : MonoBehaviour
     public GameObject m_MapObj;
     public GameObject m_PlayerObj;
     Vector3 cameraPosition = new Vector3(0, 0, -10);
-    
+    Vector3 newPosition = Vector3.zero;
+
+
     [HideInInspector] public Vector3 MapMin = Vector3.zero;
     [HideInInspector] public Vector3 MapMax = Vector3.zero;
     
@@ -21,12 +23,12 @@ public class Camera_Ctrl : MonoBehaviour
     Vector3 m_ScMax = new Vector3(1.0f,1.0f,1.0f); //ScreenViewPort 우측하단
     [HideInInspector] public Vector3 m_CamWMax = Vector3.zero;
     Vector3 m_ScWdHalf = Vector3.zero;
-    
+
+    float m_cameraMoveSpeed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
-    {
-        playerTransform = m_PlayerObj.GetComponent<Transform>();
-        
+    {        
         //--LimitMoveCam 카메라가 지형 밖으로 나가지 못하게 막기
         Vector3 a_MapHalfSize = Vector3.zero;
         
@@ -43,21 +45,53 @@ public class Camera_Ctrl : MonoBehaviour
         MapMax.x = m_MapObj.transform.position.x + a_MapHalfSize.x;
         MapMax.y = m_MapObj.transform.position.y + a_MapHalfSize.y;
         MapMax.z = m_MapObj.transform.position.z + a_MapHalfSize.z;
-        //--LimitMoveCam 카메라가 지형 밖으로 나가지 못하게 막기
+        //--LimitMoveCam 카메라가 지형 밖으로 나가지 못하게 막기r
     }
 
     // Update is called once per frame
     void Update()
     {
         //--LimitMoveCam 카메라가 지형 밖으로 나가지 못하게 막기
-        
-        
+        m_CamWMin = Camera.main.ViewportToWorldPoint(m_ScMin);//카메라 화면 좌측하단 코너의 월드좌표
+        m_CamWMax = Camera.main.ViewportToWorldPoint(m_ScMax);//카메라 화면 우측상단 코너의 월드좌표
+
+        m_ScWdHalf.x = (m_CamWMax.x - m_CamWMin.x) / 2.0f;
+        m_ScWdHalf.z = (m_CamWMax.z - m_CamWMin.z) / 2.0f;
+
+        m_LmtBdLeft = MapMin.x + 4.0f + m_ScWdHalf.x;
+        m_LmtBdTop = MapMin.z + 4.0f + m_ScWdHalf.z;
+        m_LmtBdRight = MapMax.x - 4.0f - m_ScWdHalf.x;
+        m_LmtBdBottom = MapMin.z - 4.0f - m_ScWdHalf.z;
+
+        if(m_LmtBdRight <= m_LmtBdLeft)
+            newPosition.x = transform.position.x;
+        else
+        {
+            if(newPosition.x < (float)m_LmtBdLeft)
+                newPosition.x = (float)m_LmtBdLeft;
+            
+            if ((float)m_LmtBdRight < newPosition.x)
+                newPosition.x = (float)m_LmtBdRight;
+        }
+
+        if (m_LmtBdRight <= m_LmtBdTop)
+            newPosition.z = transform.position.z;
+        else
+        {
+            if (newPosition.z < (float)m_LmtBdTop)
+                newPosition.z = (float)m_LmtBdTop;
+
+            if ((float)m_LmtBdBottom < newPosition.z)
+                newPosition.z = (float)m_LmtBdBottom;
+        }
         //--LimitMoveCam 카메라가 지형 밖으로 나가지 못하게 막기
+
+        transform.position = newPosition;
     }
     
     void FixedUpdate()
     {
-    	transform.position = Vector3.Lerp(transform.position, playerTransform.position + cameraPosition, 
-                                  Time.deltaTime * cameraMoveSpeed);
+    	transform.position = Vector3.Lerp(transform.position, m_PlayerObj.transform.position + cameraPosition, 
+                                  Time.deltaTime * m_cameraMoveSpeed);
     }
 }
